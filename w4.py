e3ccdcb41c4eb254e64a9553b59c759649cfac6a
s40523141 <40523141@gm.nfu.edu.tw>
2016-11-01 20:49:35 +0800

    　新式梁朝偉

--- w4.py
+++ w4.py
@@ -1,3 +1,10 @@
-mystring ="40523141"
-mystring = mystring + " test"
+'''
+mystring = "scrum-1444"
 print(mystring)
+'''
+
+thetext = input("Enter some text ")
+print ("This is what you entered:", "test", sep="---")
+print (thetext)
+
+
